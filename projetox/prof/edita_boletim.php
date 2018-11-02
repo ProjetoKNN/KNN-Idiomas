8ad34cbd79712b8172ce82359c7931acc29f82c5
Wes <wellingtonmoraisrx@gmail.com>
2018-11-01 22:31:02 -0300

    Perfil prof parte 4 - edita_boletim (nfunfa)/ lancarfrequencia (nfunfa)

--- projetox/prof/edita_boletim.php
+++ projetox/prof/edita_boletim.php
@@ -7,17 +7,17 @@
 			<meta charset="utf-8">
 		</head>
 		<body>
-			<a href="index_prof.php"><button>Voltar</button></a>
+			<a href="buscar_turmas.php"><button>Voltar</button></a>
 				<?php 
 					$CodAluno = filter_input(INPUT_POST, 'cod');
-					echo "AQUI: ". $CodAluno;
+					//echo "AQUI: ". $CodAluno;
 
 					$sql = "SELECT * FROM boletim where aluno_cod = '$CodAluno'";
 
 					$res = mysqli_query($conexao, $sql);
 
 					while ($dados = mysqli_fetch_assoc($res)){
-						
+						echo "Nota 1 ".$dados['nota1'];
 					}
 				?>
 			</form>
